94202f1cbbf633ba65b6aa1e162ade12ba544b7b
ArielADev <aconcepcion.dev@gmail.com>
2026-04-06 19:48:54 -0400

    Level 1 work

--- Scripts/BaseNetworkedPlayer.cs
+++ Scripts/BaseNetworkedPlayer.cs
@@ -3,11 +3,11 @@ using System;
 
 public partial class BaseNetworkedPlayer : CharacterBody3D
 {
-    [Export] protected AnimationPlayer Animator;
-    [Export] protected NetID MyId;
-    [Export] protected float baseSpeed;
+	[Export] protected AnimationPlayer Animator;
+	[Export] protected NetID MyId;
+	[Export] protected float baseSpeed;
 
-    public override void _PhysicsProcess(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
 		//GD.Print($"BNP: IsServer={GenericCore.Instance.IsServer}, IsLocal={MyId.IsLocal}, Velocity={Velocity}");
 		if (GenericCore.Instance.IsServer)
@@ -27,27 +27,27 @@ public partial class BaseNetworkedPlayer : CharacterBody3D
 		AllProcess((float)delta);
 	}
 
-    void OnCollisionEntered(Node collider)
-    {
-        if (GenericCore.Instance.IsServer)
-            ServerCollisionEvent(collider);
-        if (MyId.IsLocal)
-            LocalCollisionEvent(collider);
-        if (!GenericCore.Instance.IsServer)
-            AllPlayerCollisionEvent(collider);
+	void OnCollisionEntered(Node collider)
+	{
+		if (GenericCore.Instance.IsServer)
+			ServerCollisionEvent(collider);
+		if (MyId.IsLocal)
+			LocalCollisionEvent(collider);
+		if (!GenericCore.Instance.IsServer)
+			AllPlayerCollisionEvent(collider);
 
-        AllCollisionEvent(collider);
-    }
+		AllCollisionEvent(collider);
+	}
 
-    //------ Process Handlers ------\\
-    public virtual void ServerProcess(float delta) {}
-    public virtual void LocalProcess(float delta) {}
-    public virtual void AllPlayerProcess(float delta) {}
-    public virtual void AllProcess(float delta) {}
+	//------ Process Handlers ------\\
+	public virtual void ServerProcess(float delta) {}
+	public virtual void LocalProcess(float delta) {}
+	public virtual void AllPlayerProcess(float delta) {}
+	public virtual void AllProcess(float delta) {}
 
-    //------ Collision Handlers -------\\
-    public virtual void ServerCollisionEvent(Node collider) {}
-    public virtual void LocalCollisionEvent(Node collider) {}
-    public virtual void AllPlayerCollisionEvent(Node collider) {}
-    public virtual void AllCollisionEvent(Node collider) {}
+	//------ Collision Handlers -------\\
+	public virtual void ServerCollisionEvent(Node collider) {}
+	public virtual void LocalCollisionEvent(Node collider) {}
+	public virtual void AllPlayerCollisionEvent(Node collider) {}
+	public virtual void AllCollisionEvent(Node collider) {}
 }
